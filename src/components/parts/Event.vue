ac0db4eb2679bd61509650400e5670beb4a1e091
YadaYuki <yada.yuki@fuji.waseda.jp>
2021-01-17 17:48:43 +0900

    add event components

---
+++ src/components/parts/Event.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="fuga">
+    <ProfileSubsectionTitle subsection_title="Internship・Event" />
+    <!-- For -->
+
+      <EventItem v-for="event in events" v-bind:event="event" :key="event.id" />
+
+  </div>
+</template>
+<script>
+import ProfileSubsectionTitle from "@/components/parts/ProfileSubsectionTitle.vue";
+import EventItem from "@/components/parts/EventItem.vue";
+import events from "@/assets/data/event.json";
+
+export default {
+  name: "Event",
+  components: {
+    ProfileSubsectionTitle,
+    EventItem,
+  },
+  data: () => {
+    return { events };
+  },
+};
+</script>
+<style lang="scss" scoped>
+div {
+margin-top:40px;
+
+}
+</style>
